4e89a5a77fb838fc563d100b7109db639e312b3d
poojamundada <pooja.mundada@ggktech.com>
2019-12-26 12:47:04 +0530

    clonefirsttime

---
+++ TestNG/src/test/java/Maven/TestNG/AppTest.java
@@ -0,0 +1,18 @@
+package Maven.TestNG;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       System.out.print("Jenkins Integration with Selenium" );
+    }
+}
